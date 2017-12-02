1a366068b29b01d0a16626a008c3592dedfa430d
A7xSV <sabhya.verma16@gmail.com>
2017-12-03 02:53:28 +0530

    add fix

--- CSV Modifier/simultaneous_files_search.py
+++ CSV Modifier/simultaneous_files_search.py
@@ -1,27 +1,26 @@
 import csv
 
-reader = csv.reader(open('pacer_cases.csv', 'rb'))
-read_file = open('cases_formatted.csv', 'rb');
+reader = csv.reader(open('p_c.csv', 'rb'))
+read_file = open('c_f.csv', 'rb');
 reader_search = csv.reader(read_file)
-names_file = open('names_formatted.csv', 'rb');
+names_file = open('n_f.csv', 'rb');
 names_search = csv.reader(names_file)
 
-def search_names(case_row_id, case_num):
+def search_names(case_id, case_num):
     names_file.seek(0)
     for row in names_search:
-        if case_row_id == row[0] and case_num == row[1]:
+        if case_id == row[0] and case_num == row[1]:
             print row
 
-def search_for(court_name, case_num):
+def search_for(court_n, case_num):
     read_file.seek(0)
-    pacer_court_name = court_name.split()[0].lower()
-    print pacer_court_name, " ", case_num
+    pacer_court_n = court_name.split()[0].lower()
+    print pacer_court_n, " ", case_num
     for row in reader_search:
-        if pacer_court_name in row[4].lower():
+        if pacer_court_n in row[4].lower():
             if (case_num == row[1]):
                 print row
                 search_names(row[0], row[1])
-                # return
 
 i = 0;
 for row in reader:
